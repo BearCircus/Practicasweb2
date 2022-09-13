d05eeeced19902b052f04b0a115e759e3f3d7c4d
Isaac Castillo <isaac.castilloherrera036@gmail.com>
2022-09-13 18:47:03 -0500

    news on html

--- Practica1/src/routes/noticias.js
+++ Practica1/src/routes/noticias.js
@@ -1,20 +1,20 @@
 const axios = require("axios");
 const app = require("express")();
 const router = require("express").Router();
-const { engine } = require("express-handlebars");
 
 async function getNews(req, res) {
+  encodeURI();
   let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.API_KEY}`;
   const resp = await axios.get(url);
   const data = resp.data;
-  // console.log(data);
+  return data;
 }
 
-app.get("/", (req, res) => {
-  const news = getNews(req, res);
-  console.log(typeof news);
-  // res.render("index", { title: news });
-  res.send(news);
+app.get("/", async (req, res) => {
+  const news = await getNews(req, res);
+  const articles = news.articles;
+  res.send(articles);
+  // res.render("index", { title: articles });
 });
 
 module.exports = app;
